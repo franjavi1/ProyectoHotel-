eaebef969430f0520b6a76b8c21b3571f0daebe5
Luis <Luis@DESKTOP-PAIKIR3>
2025-11-01 16:32:45 -0300

    modificando estadisticas

--- HotelApp/obj/Debug/net8.0/HotelWeb.AssemblyInfo.cs
+++ HotelApp/obj/Debug/net8.0/HotelWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4044e4a23a1860864f86766469228a98c1584494")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+034b55c0279156a84ca2913e14b4855b8966e262")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
